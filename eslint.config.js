34c89c3ffd79f83327d0460df82a38a9cfcfcd65
Mourad <mouradbougarne@gmail.com>
2024-11-09 17:54:20 +0100

    config eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,28 @@
-import js from '@eslint/js'
-import globals from 'globals'
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import eslintPrettier from 'eslint-config-prettier'
 
+/** @type {import('eslint').Linter.Config[]} */
 export default tseslint.config(
-  { ignores: ['dist'] },
   {
-    extends: [
-      js.configs.recommended,
-      'airbnb',
-      'airbnb/hooks',
-      'plugin:react/recommended',
-      ...tseslint.configs.recommended
-    ],
+    ignores: ['dist', 'public', 'node_modules']
+  },
+  {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    extends: [
+      pluginJs.configs.recommended,
+      ...tseslint.configs.recommended,
+      pluginReact.configs.flat.recommended,
+      eslintPrettier,
+    ],
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
@@ -29,6 +33,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'react/react-in-jsx-scope': 'off',
     },
   },
 )
