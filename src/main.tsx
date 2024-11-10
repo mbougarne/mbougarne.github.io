e22132f1fa9e898a3f774f33c4617bec60ae1f6c
Mourad <mouradbougarne@gmail.com>
2024-11-10 10:43:14 +0100

    Add Icon, Inter font, and svg icons

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { App } from './App';
+
+// Supports weights 100-900
+import '@fontsource-variable/inter/opsz-italic.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
